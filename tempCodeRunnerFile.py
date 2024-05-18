b13071700df0732cec579ccb88bb8fdc67a5ce56
Ritesh0upadhyay <enjeenierritesh1234@gmail.com>
2024-05-18 14:55:20 +0530

    re-init

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-if __name__ == "__main__":
-    speak("please speak the password")
-    while True:
-        m1 = takeCommand()
-        if "i am here" in m1:
-            body()
-        else:
-            speak("sory sir the password is not correct")
-        
--- whatsappmessage.py
+++
@@ -1,6 +0,0 @@
-elif 'send message' in query:
-            dm = takecommand().lower()
-            r = int(takecommand().lower())
-            
-
-            kit.sendwhatmsg(f"{r}",f"{dm}")
